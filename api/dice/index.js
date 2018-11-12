4361917ce51f16536b6f3f74c2c0fddfd959cd50
ctrlaltcookie <jack.philip.butler@gmail.com>
2018-11-12 19:51:33 +0000

    added restrictions to the dice to prevent thread locking and potential monetary cost of large iterations

--- api/dice/index.js
+++ api/dice/index.js
@@ -6,6 +6,9 @@ const Util = require('../util');
  * @param {*} iterations Number of times to throw
  */
 const roll = function (dice, iterations) {
+  if (dice >= 100000000 || iterations >= 10000) {
+    return ` - nothing, cos you were being a dick, dice don't need 100000000 sides nor do you need to roll them 10k times, be sensible.`;
+  }
   let rolls = [];
   for(let i = 0; i < iterations; i++) {
     rolls.push(Util.getRand(dice));
